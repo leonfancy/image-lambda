990022dbadd2705fd8b3fb800732f8ef686678ba
Chen Liang <slimhigh@gmail.com>
2016-05-14 22:12:11 +0800

    Add integration test.

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ exports.handler = function (event, context, callback) {
     let srcBucket = s3Record.bucket.name;
     let srcKey = decodeURIComponent(s3Record.object.key.replace(/\+/g, " "));
     let config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "config.json"), {encoding: "utf8"}));
+    let s3Client = new S3Client();
 
-    S3Client.getS3Image(srcBucket, srcKey)
+    s3Client.getS3Image(srcBucket, srcKey)
         .then(function (image) {
             let processor = new ImageProcessor(image, config);
             return processor.run();
         })
-        .then(S3Client.putS3Images)
+        .then(function(images) {
+            return s3Client.putS3Images(images);
+        })
         .then((results) => {
             var message = util.format("[%s] [%s] - %s", srcBucket, srcKey,
                 `Successfully processed. Generated ${results.length} images.`);
---
+++ test/integration.test.js
@@ -0,0 +1,33 @@
+'use strict'
+let fs = require("fs");
+let path = require("path");
+let AWS = require('aws-sdk-mock');
+let expect = require("chai").expect;
+
+let lambdaHandler = require("../index").handler;
+
+describe("Handle s3 event", function () {
+    this.timeout(10000);
+    before(function () {
+        AWS.mock("S3", "getObject", function (params, callback) {
+            fs.readFile(path.join(__dirname, "fixture/jpg/girl-2560x1600-1.3MB.jpg"), function (error, content) {
+                callback(error, {Body: content, ContentType: "image/jpeg"});
+            });
+        });
+        AWS.mock("S3", "putObject", function (params, callback) {
+            callback(null);
+        });
+    });
+
+    it("should process image", function (done) {
+        let eventFixture = JSON.parse(fs.readFileSync(path.join(__dirname, "/fixture/event.json")));
+        lambdaHandler(eventFixture, null, (error, data) => {
+            if (error) {
+                console.error(error);
+            } else {
+                done();
+            }
+        });
+    })
+});
+
